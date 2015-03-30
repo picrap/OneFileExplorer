b147a15396a77841cc1780e04b46d5ede915d121
picrap <pascal@craponne.fr>
2015-03-30 17:15:19 +0200

    Added project
    But empty (got problems)

---
+++ OneFileExplorer/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("OneFileExplorer")]
+[assembly: AssemblyDescription("FileExplorer Profile for OneFileSystem")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("craponne.fr :)")]
+[assembly: AssemblyProduct("OneFileExplorer")]
+[assembly: AssemblyCopyright("Released under MIT license - http://opensource.org/licenses/MIT")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.1")]
+
