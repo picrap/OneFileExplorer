7e34b200ab7b13d089028afa250b8c4bea12841b
Pascal Craponne <pascal@craponne.fr>
2015-05-04 08:43:51 +0200

    Added test app

---
+++ OneFileExplorerTest/OneFileExplorerApplication.xaml.cs
@@ -0,0 +1,23 @@
+﻿// OneFileExplorer
+// A FileExplorer model for use with OneFilesystem
+// https://github.com/picrap/OneFileExplorer
+
+namespace OneFileExplorerTest
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class OneFileExplorerApplication
+    {
+        public OneFileExplorerApplication()
+        {
+            Startup += OnStartup;
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+        }
+    }
+}
