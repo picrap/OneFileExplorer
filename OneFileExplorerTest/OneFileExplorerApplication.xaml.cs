11e2cc529e1a4ae6fd9bbe745a961a1768d9221b
Pascal Craponne <pascal@craponne.fr>
2015-05-08 22:42:54 +0200

    Experiments
    (non working)

--- OneFileExplorerTest/OneFileExplorerApplication.xaml.cs
+++ OneFileExplorerTest/OneFileExplorerApplication.xaml.cs
@@ -4,8 +4,12 @@
 
 namespace OneFileExplorerTest
 {
+    using System.IO;
     using System.Windows;
     using Caliburn.Micro;
+    using FileExplorer.IO;
+    using FileExplorer.Models;
+    using FileExplorer.WPF.Models;
     using FileExplorer.WPF.ViewModels;
     using FileExplorer.WPF.Views;
     using OneFileExplorer;
@@ -20,12 +24,19 @@ namespace OneFileExplorerTest
             Startup += OnStartup;
         }
 
-        private void OnStartup(object sender, StartupEventArgs e)
+        private async void OnStartup(object sender, StartupEventArgs e)
         {
+            var windowManager = new WindowManager();
             var view = new OneFileExplorerWindow();
-            var viewModel = new ExplorerViewModel(null, new EventAggregator());
+            var eventAggregator = new EventAggregator();
+            var viewModel = new ExplorerViewModel(windowManager, eventAggregator);
+            viewModel.RootProfiles = new IProfile[] { new FileSystemInfoExProfile(eventAggregator, windowManager), };
+            var root = await viewModel.RootProfiles[0].ParseAsync(DirectoryInfoEx.DesktopDirectory.FullName);
+            viewModel.RootModels = new IEntryModel[] { root };
             view.DataContext = viewModel;
-            view.ShowDialog();
+            //view.ShowDialog();
+            windowManager.ShowPopup(viewModel);
+            //windowManager.ShowWindow(viewModel);
         }
     }
 }
